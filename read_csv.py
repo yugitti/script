e43cec0825229beb9dd30bdac04090b50112ff79
yugi <yugitti@gmail.com>
2018-03-23 21:55:36 +0900

    get human face image from open image dataset

--- read_csv.py
+++ read_csv.py
@@ -3,70 +3,172 @@ import csv
 from memory_profiler import profile
 import pandas as pd
 
-
-def proc():
-    start_time = time.time()
-    d = []
-    count = 0
-    with open('./csv_file/test/annotations-human.csv') as f:
-        read = csv.reader(f)
-
-        for r in read:
-            d.append(r)
-            count += 1
-
-    end_time = time.time()
-
-    elapsed_time = end_time - start_time
+# PATH = "./classes/class-descriptions.csv"
+PATH_CLASS = "./classes/class-descriptions.csv"
+PATH_ANNOT = "./annotations/validation/annotations-human.csv"
+PATH_OUT_ANNOT_FACE = "./annotations/validation/annotations-face.csv"
+PATH_IMAGE_URL = "./images/validation/images.csv"
+PATH_OUT_IMAGE_URL = "./images/train/images-face.csv"
 
 
+HEADER = ['ImageID','Subset','OriginalURL','OriginalLandingURL','License','AuthorProfileURL','Author','Title','OriginalSize','OriginalMD5','Thumbnail300KURL']
+MOUTH_ID = '/m/0283dt1'
+EYE_ID = '/m/014sv8'
+FACE_ID = '/m/0dzct'
+HUMAN_ID = '/m/01g317'
 
-    return elapsed_time, count, len(d)
+def myTime(func):
+    import functools
+    import datetime
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        start = datetime.datetime.today()
+        result = func(*args, **kwargs)
+        end = datetime.datetime.today()
+        return end - start
+    return wrapper
+
+
+def get_class_name():
+    read = pd.read_csv(PATH_CLASS)
+    temp = read[read['name'].str.contains('Human|Face|Eye|Mouth')]
+    print temp
+
+# @myTime
+# @profile
+def get_label1(read):
+    temp = read[read['LabelName'].str.contains('%s|%s|%s'%(FACE_ID,EYE_ID,MOUTH_ID))]
+    print temp.shape
+    return temp
+# @myTime
+# @profile
+def get_label2(read, SEARCH_ID):
+    # temp = read.query('LabelName==@FACE_ID | LabelName==@EYE_ID | LabelName==@MOUTH_ID | LabelName==@ ')
+    temp = read.query('LabelName==@SEARCH_ID')
+    # print temp.shape
+    return temp
+# @myTime
+# @profile
+def get_label3(read):
+    temp = read[(read['LabelName'] == FACE_ID)|(read['LabelName'] == EYE_ID)|(read['LabelName'] == MOUTH_ID)]
+    print temp.shape
+    return temp
+
+def get_matach_imageID(df_word, df_target):
+    col = df_word.columns
+    df = pd.DataFrame(columns=col)
+    hit_count = 0
+    for k, a in enumerate(df_word.itertuples()):
+        image_id = a[1]
+
+        if not (df_target.query('ImageID==@image_id')).empty:
+            df = df.append(df_word.iloc[k])
+            # hit_count += 1
+            # if (hit_count % 100 == 0):
+            #     print hit_count
+
+    return df
+
+
+def get_imageID2():
+    read = pd.read_csv(PATH_ANNOT)
+    col = read.columns
+    human = pd.DataFrame(columns=col)
+    print "get duplicate"
+    a = read.duplicated(subset='ImageID')
+    ind = []
+    hit_count = 0
+
+    print "get index"
+    for i, aa in enumerate(a):
+        if (aa == False):
+            ind.append(i)
+    sf = read['LabelName']
+
+    print "start query"
+    for k, top_id in enumerate(ind):
+        if (k != len(ind) - 1):
+            temp = sf[top_id: ind[k + 1]]
+            temp2 = temp.str.contains('%s|%s|%s|%s'%(HUMAN_ID,FACE_ID,EYE_ID,MOUTH_ID)).value_counts(sort=False)
+            if (len(temp2) == 4):
+                human = human.append(read.iloc[top_id])
+                hit_count += 1
+                if(hit_count%100 == 0):
+                    print hit_count
+
+    human_opt_id = pd.DataFrame(human['ImageID'], columns=['ImageID'])
+    print human_opt_id.shape
+    return human_opt_id
+
 
 # @profile
-def proc2():
-    start_time = time.time()
-    d = []
+def get_imageID():
+    read = pd.read_csv(PATH_ANNOT)
+    col = read.columns
+    human = pd.DataFrame(columns=col)
+    person = get_label2(read, HUMAN_ID)
+    face = get_label2(read, FACE_ID)
+    eye = get_label2(read, EYE_ID)
+    mouth = get_label2(read, MOUTH_ID)
+
+    print person.shape
+    print face.shape
+    print eye.shape
+    print mouth.shape
+
+    print "mouth shape is %d" %mouth.shape[0]
+    temp = get_matach_imageID(mouth, eye)
+    print "mouth w/ eye shape is %d" % temp.shape[0]
+    temp = get_matach_imageID(temp, face)
+    print "mouth w/ eye&face shape is %d" % temp.shape[0]
+    human = get_matach_imageID(temp, person)
+    print "person w/ mouth&eye&face shape is %d" % human.shape[0]
+
+    human_opt = human.drop_duplicates(subset='ImageID')
+
+    # for a in human_opt.itertuples():
+    #     print a[1]
+    human_opt_id = pd.DataFrame(human_opt['ImageID'], columns=['ImageID'])
+    print human_opt_id.shape
+    # print human_opt_id[0:10]
+    return human_opt_id
+
+# @profile
+def get_imageURL(id_table):
+    # df = pd.Series()
+    df = pd.DataFrame(columns=HEADER)
+    print type(df)
     count = 0
+    hit_count = 0
 
-    read = pd.read_csv('./csv_file/test/annotations-human.csv')
-    for r in read:
-        d.append(r)
-        count += 1
+    reads = pd.read_csv(PATH_IMAGE_URL, chunksize=10000)
+    i = 1
+    for read in reads:
+        print "no %d" %i
+        i = i + 1
+        for k, a in enumerate(read.itertuples()):
+            image_id = a[1]
+            # print image_id
+            temp = id_table.query('ImageID==@image_id')
+            if not temp.empty:
+                df = df.append(read.iloc[k])
+                index = temp.index
+                id_table.drop(index)
+                hit_count += 1
+                if(hit_count%100 == 0):
+                    print hit_count
 
-    end_time = time.time()
-    elapsed_time = end_time - start_time
+    print df.shape
+    return df
 
-    print(type(read))
-    # print(read.shape)
-
-    return elapsed_time, count, len(d)
-
-@profile
-def proc3():
-    start_time = time.time()
-    d = []
-    count = 0
-
-    read = pd.read_csv('./csv_file/test/annotations-human.csv', chunksize=10000)
-
-    for rr in read:
-        # print rr.shape
-        for r in range(rr.shape[0]):
-            d.append(r)
-            count += 1
-
-    end_time = time.time()
-    elapsed_time = end_time - start_time
-
-    return elapsed_time, count, len(d)
 
 if __name__ == '__main__':
 
-    elapsed_time, count, l = proc2()
+    # get_class_name()
+    human_id = get_imageID()
+    image_human_id = get_imageURL(human_id)
+    human_id.to_csv(PATH_OUT_ANNOT_FACE, index=False)
+    image_human_id.to_csv(PATH_OUT_IMAGE_URL, index=False)
 
-    print "time: %f" % elapsed_time
-    print "count: %d" % count
-    print "length: %d" % l
 
 
--- scraping_practice.py
+++ scraping_practice.py
@@ -1,31 +1,45 @@
-import cv2
+import time
 import urllib2
 from bs4 import BeautifulSoup
+import requests
+import pandas as pd
 
-URL = "http://www.yahoo.co.jp"
-USERAGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) '\
-     'AppleWebKit/537.36 (KHTML, like Gecko) '\
-     'Chrome/55.0.2883.95 Safari/537.36 '
+PATH_INPUT_IMAGE_URL = "./images/test/images-face.csv"
+PATH_OUT_IMAGE = "./images/test/img/"
 
-req = urllib2.Request(URL)
-req.add_header("User-agent", USERAGENT)
-
-html = urllib2.urlopen(req)
-soup = BeautifulSoup(html, "html.parser")
-
-topicsindex = soup.find('div', attrs= {'class': 'topicsindex'})
-topics = topicsindex.find_all('li')
-
-for top in topics:
-     print top.find('a').contents[1]
-
-
-
-
-
-
-
+# URL = "http://www.yahoo.co.jp"
+# USERAGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) '\
+#      'AppleWebKit/537.36 (KHTML, like Gecko) '\
+#      'Chrome/55.0.2883.95 Safari/537.36 '
+#
+# req = urllib2.Request(URL)
+# req.add_header("User-agent", USERAGENT)
+#
+# html = urllib2.urlopen(req)
+# soup = BeautifulSoup(html, "html.parser")
+#
+# topicsindex = soup.find('div', attrs= {'class': 'topicsindex'})
+# topics = topicsindex.find_all('li')
+#
+# for top in topics:
+#      print top.find('a').contents[1]
 
 
+read = pd.read_csv(PATH_INPUT_IMAGE_URL)
+url = read['OriginalURL']
+print url[0:10]
 
+fName = url[0].split('/')[-1]
+suffix = fName.split('.')[-1]
+file_name = 0
 
+for img in url:
+     print img
+     image_name = img.split('/')[-1]
+     suffix = image_name.split('.')[-1]
+     re = requests.get(img)
+     with open(PATH_OUT_IMAGE + '%05d'%file_name + suffix, 'wb') as f:
+          f.write(re.content)
+     file_name += 1
+
+     time.sleep(1)
